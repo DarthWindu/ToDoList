968f9888d40e5f8b9e3cd7afa332b3b5e8ed9d07
suttonr75 <suttonr75@gmail.com>
2017-02-24 08:51:25 -0500

    comitting Pujit's code

---
+++ ToDoList/src/PriorityChange.java
@@ -0,0 +1,49 @@
+package backend;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 
+ * @author Pujit
+ * 
+ * @version p 0.1
+ * 
+ * Represents Priority change Date Item
+ *
+ */
+
+//DOESNT THIS NEED A DATE FIELD??? - I added one
+public class PriorityChange extends HistoryItem {
+	Integer oldStatus, newStatus;
+	
+	PriorityChange(int userOldStatus, int userNewStatus)
+	{
+		this(userOldStatus, userNewStatus, Calendar.getInstance().getTime());//Is this the correct way to get today's date (plus hh.mm am/pm time)?
+	}
+	
+	PriorityChange(int userOldStatus, int userNewStatus, Date dateOfEvent)
+	{
+		oldStatus = new Integer(userOldStatus);
+		newStatus = new Integer(userNewStatus);
+		this.setDate(dateOfEvent);
+	}
+	
+	/**
+	 * @author Pujit
+	 * @return oldStatus Value
+	 */
+	public int getOldStatus()
+	{
+		return oldStatus.intValue();
+	}
+	
+	/**
+	 * @author Pujit
+	 * @return newStatus Value
+	 */
+	public int getNewStatus()
+	{
+		return newStatus.intValue();
+	}
+}
