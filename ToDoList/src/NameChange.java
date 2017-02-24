9cef4138dfb9d35cb9119ce9477dc7823cad5a02
suttonr75 <suttonr75@gmail.com>
2017-02-24 08:53:40 -0500

    committing Pujit's code

---
+++ ToDoList/src/NameChange.java
@@ -0,0 +1,33 @@
+package backend;
+
+import java.util.Calendar;
+/**
+ * 
+ * @author Pujit
+ * 
+ * @version p 0.1
+ * 
+ * Represents a name change event
+ * No changes, except for possible addition of serialization implementation is expected.
+ *
+ */
+public class NameChange extends HistoryItem {
+	private String oldName, newName;
+	
+	NameChange(String old_Name, String new_Name)
+	{
+		oldName = old_Name;
+		newName = new_Name;
+		this.setDate(Calendar.getInstance().getTime());
+	}
+	
+	public String getOldName()
+	{
+		return oldName;
+	}
+	
+	public String getNewName()
+	{
+		return newName;
+	}
+}
