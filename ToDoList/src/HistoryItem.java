e2b8f8f3d3710ab642e0024fc5c789fea7a59812
suttonr75 <suttonr75@gmail.com>
2017-02-24 08:46:01 -0500

    commiting Pujit's code

---
+++ ToDoList/src/HistoryItem.java
@@ -0,0 +1,35 @@
+package backend;
+
+import java.util.Date;
+/**
+ * 
+ * @author Pujit
+ * 
+ * @version p 0.1 (p is for prototype, v is for version)
+ * 
+ * Represents an event that should be displayed in a task's history
+ *
+ */
+public abstract class HistoryItem {
+	protected Date whenDone;
+	
+	/**
+	 * @author Pujit
+	 * @return Date whenDone: When the HistoryItem event took place
+	 * Updated p 0.1
+	 */
+	public Date getDate() {
+		return whenDone;
+	}
+	
+	/**
+	 * @author Pujit
+	 * @param date: Date HistoryItem takes/took/will take place
+	 * May be unecessary
+	 * 
+	 * Updated p 0.1
+	 */
+	public void setDate (Date date) {
+		whenDone = date;
+	}
+}
