d2a220bee24200c9afa0313071211f9ebf431e61
DuhMastuhCoduh <taiquanishalafonza@gmail.com>
2017-02-27 09:04:57 -0500

    my commit

--- ToDoList/src/frontend/EditActionItem.java
+++ ToDoList/src/frontend/EditActionItem.java
@@ -1,6 +1,14 @@
 package frontend;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class EditActionItem extends JPanel{
+	JRadioButton radioButton;
+	JCheckBox checkCurent,checkUrgent,checkEventual;
+	JButton commentButton;
+	JButton historyButton;
+	JButton print;
 
+	EditActionItem() {
+		
+	}
 }
