3efb0caa7d065277c8b30602e53663ae4c327d4a
JoeCleary <joseph.david.cleary@gmail.com>
2017-03-01 09:29:25 -0500

    rightcluck

--- ToDoList/src/frontend/RightClickMenu.java
+++ ToDoList/src/frontend/RightClickMenu.java
@@ -1,15 +1,20 @@
 package frontend;
 import backend.*;
+
 import javax.swing.*;
+
+import java.awt.*;
 import java.awt.event.*;
 
 public class RightClickMenu extends JPanel implements ActionListener{
-	JMenu menu;
+	Menu menu;
 	JMenuItem[] menuItems;
 	
 	
 	RightClickMenu() {
+		menu = new Menu("right");
 		
+		menuItems = new JMenuItem[3];
 	}
 	
 	public void actionPerformed(ActionEvent arg0) {}
