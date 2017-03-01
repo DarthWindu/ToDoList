3faed134ff89cafb5fa9495d4bfe46ff7e84867a
mailman2018 <meatball951>
2017-03-01 08:44:19 -0500

    Tester class holds date format

--- ToDoList/src/frontend/Tester.java
+++ ToDoList/src/frontend/Tester.java
@@ -1,12 +1,11 @@
 package frontend;
-import backend.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 public class Tester {
-
+    public static void main(String[] args) {
+    	Date date = Calendar.getInstance().getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm aa");
+        System.out.println(sdf.format(date));
+    }
 }
- /*.addActionListener(new ActionListener(){
-public void actionPerformed(ActionEvent arg0) {
-	}
-		});*/
