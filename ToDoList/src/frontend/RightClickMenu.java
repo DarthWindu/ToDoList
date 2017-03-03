4d057fd344d2e103d9cf9d4619870cf6523b46f1
JoeCleary <joseph.david.cleary@gmail.com>
2017-03-03 09:08:22 -0500

    RightClickMenu stuff

--- ToDoList/src/frontend/RightClickMenu.java
+++ ToDoList/src/frontend/RightClickMenu.java
@@ -1,15 +1,50 @@
 package frontend;
 import backend.*;
+
 import javax.swing.*;
+
 import java.awt.event.*;
 
-public class RightClickMenu extends JPanel implements ActionListener{
-	JMenu menu;
-	JMenuItem[] menuItems;
+public class RightClickMenu extends JPanel{
+	JPopupMenu menu;
 	
-	
-	RightClickMenu() {
+	RightClickMenu(int x, int y) {
+		menu = new JPopupMenu();
 		
+		JMenuItem completeItem = new JMenuItem("Set priority to completed");
+		
+		completeItem.addActionListener(new ActionListener(){//might need to use popupmenulisteners
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+			}
+		});
+		
+		JMenuItem deleteItem = new JMenuItem("Delete");
+		
+		deleteItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+			}
+		});
+		
+		JMenuItem editItem = new JMenuItem("Edit");
+		
+		editItem.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				
+				
+			}
+		});
+		
+		menu.add(completeItem);
+		
+		menu.add(deleteItem);
+		
+		menu.add(editItem);
+
+		menu.setVisible(true);
 	}
 	
 	public void actionPerformed(ActionEvent arg0) {}
