cde5a339bcc8b72b881f27b91c25ff37b4da0196
mailman2018 <meatball951>
2017-03-03 09:06:50 -0500

    CommentWindow

--- ToDoList/src/frontend/CommentWindow.java
+++ ToDoList/src/frontend/CommentWindow.java
@@ -1,29 +1,29 @@
 package frontend;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class CommentWindow extends JFrame{
 	JButton commit, delete;
 	JFrame frame;
+	JTextField field;
+	JPanel panel;
 	CommentWindow() {
 		frame = new JFrame("Comment Window");
 		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		panel = new JPanel();
+		field = new JTextField();
+		panel.add(field,BorderLayout.NORTH);
+		JButton commit = new JButton("Commit");
+		commit.setBounds(50, 50, 50, 50);
+		panel.add(commit);
+		frame.setContentPane(panel);
+		frame.setSize(1600,900);
+		frame.pack();
 		frame.setVisible(true);
-		commit = new JButton();
-		commit.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		delete.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
 	}
 	public static void main(String[] args){
 		CommentWindow test = new CommentWindow();
