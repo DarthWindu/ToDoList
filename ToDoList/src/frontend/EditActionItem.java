99b2ca7995f2d388ee8f8cd917fa00e5e8ef7329
Josh <josh.shin2000@gmail.com>
2017-03-06 08:55:34 -0500

    EditActionItem updated

--- ToDoList/src/frontend/EditActionItem.java
+++ ToDoList/src/frontend/EditActionItem.java
@@ -6,49 +6,46 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class EditActionItem extends JPanel implements ActionListener{
-	JRadioButton radioButton;
+	JRadioButton radioButton, radioButton2, radioButton3, radioButton4;
 	JCheckBox checkCurrent,checkUrgent,checkEventual;
 	JButton commentButton;
 	JButton historyButton;
 	JButton print;
 
-	EditActionItem(Task task) {
-		radioButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
+	EditActionItem(Task task)
+	{
+		ButtonGroup group = new ButtonGroup();
+		
+		radioButton = new JRadioButton("Urgent");
+		radioButton2 = new JRadioButton("Current");
+		radioButton3 = new JRadioButton("Eventual");
+		radioButton4 = new JRadioButton("Inactive");
 
-			}
-		});
-		checkCurrent.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
+		group.add(radioButton);
+		group.add(radioButton2);
+		group.add(radioButton3);
+		group.add(radioButton4);
 
-			}
-		});
-		checkUrgent.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		checkEventual.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		commentButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		historyButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
-		print.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e) {
-
-			}
-		});
+		this.add(radioButton);
+		this.add(radioButton2);
+		this.add(radioButton3);
+		this.add(radioButton4);
+	
+		
 	}
 
-	public void actionPerformed(ActionEvent e) {}
+	public void actionPerformed(ActionEvent e)
+	{
+		
+	}
+
+	public static void main(String [] args)
+	{
+		JFrame frame = new JFrame("This");
+		Task e = new Task("NameOfTask");
+		EditActionItem x = new EditActionItem(e);
+		frame.setContentPane(x);
+		frame.setVisible(true);
+		frame.pack();
+	}
 }
