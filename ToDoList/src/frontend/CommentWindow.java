ffbf6b92346523d696b04fc55591ef487c43b3d9
mailman2018 <meatball951>
2017-03-06 09:06:47 -0500

    worked on CommentWindow

--- ToDoList/src/frontend/CommentWindow.java
+++ ToDoList/src/frontend/CommentWindow.java
@@ -1,31 +1,41 @@
 package frontend;
-import java.awt.BorderLayout;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+public class CommentWindow {
+   private JFrame mainFrame;
+   private JLabel statusLabel;
+   private JPanel controlPanel;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+   public CommentWindow(){
+      prepareGUI();
+   }
+   public static void main(String[] args){
+      CommentWindow swingControlDemo = new CommentWindow();      
+   }
+   private void prepareGUI(){
+      mainFrame = new JFrame("Comment Window");
+      mainFrame.setSize(500,200);
+      mainFrame.setLayout(new GridLayout(0,3));      
 
-public class CommentWindow extends JFrame{
-	JButton commit, delete;
-	JFrame frame;
-	JTextField field;
-	JPanel panel;
-	CommentWindow() {
-		frame = new JFrame("Comment Window");
-		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		panel = new JPanel();
-		field = new JTextField();
-		panel.add(field,BorderLayout.NORTH);
-		JButton commit = new JButton("Commit");
-		commit.setBounds(50, 50, 50, 50);
-		panel.add(commit);
-		frame.setContentPane(panel);
-		frame.setSize(1600,900);
-		frame.pack();
-		frame.setVisible(true);
+
+      controlPanel = new JPanel();
+      controlPanel.setLayout(new FlowLayout());
+
+      mainFrame.add(controlPanel);
+      mainFrame.setVisible(true);  
+      JLabel  namelabel= new JLabel("Comment", JLabel.RIGHT);
+      JTextField userText = new JTextField(6); 
+      userText.setPreferredSize( new Dimension(200,24));
+
+      JButton commit = new JButton("Commit Comment");
+      JButton delete = new JButton("Delete Comment");
+      controlPanel.add(namelabel);
+      controlPanel.add(userText);
+      controlPanel.add(commit);
+      controlPanel.add(delete);
+      mainFrame.setVisible(true);  
+   }
 	}
-	public static void main(String[] args){
-		CommentWindow test = new CommentWindow();
-	}
-}
+
