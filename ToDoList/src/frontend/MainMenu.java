c7de8dae69baa1c46643109c3551ddd09e0d3134
JoeCleary <joseph.david.cleary@gmail.com>
2017-03-06 19:56:57 -0500

    mainmenu now has big scroll pane and textfield

--- ToDoList/src/frontend/MainMenu.java
+++ ToDoList/src/frontend/MainMenu.java
@@ -1,49 +1,84 @@
 package frontend;
-import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseWheelListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.JMenuBar;
 
 import backend.Task;
 import backend.ToDoList;
 
 public class MainMenu extends JPanel implements MouseListener,MouseWheelListener{
-	JTextField checkEnter;
+	JTextField addTask;
 	JFrame frame;
 	JPanel panel;
 	JScrollPane scrollPane;
+	JMenuBar bar;
 	ArrayList<Task> activeTasks;
 	private static ToDoList toDoList;
 	final static int WIDTH = 1000;
 	final static int HEIGHT = 800;
-	MainMenu() {
-		toDoList = new ToDoList();
-		checkEnter.addActionListener(new ActionListener(){
+	MainMenu(ToDoList list) {
+		
+		if(list == null)
+			toDoList = new ToDoList();
+		else
+			toDoList = list;
+		
+		
+		
+		
+		
+		addTask = new JTextField();
+		addTask.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 
 			}
 		});
+		
 		frame = new JFrame("babushka");
+		
+		
+		frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
+		
+		
+		
+		
+		
+		frame.addWindowListener(new WindowAdapter() {
+			  public void windowClosing(WindowEvent e) {
+				  //make sure that all save operations are done
+			  }
+		});
+		
+		
+		
 		panel = new JPanel();
 		panel.setPreferredSize(new Dimension(WIDTH,HEIGHT*2));
+		
 		scrollPane = new JScrollPane(panel);
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		
+		
 		scrollPane.setVisible(true);
+		
 		frame.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		
 		scrollPane.setPreferredSize(new Dimension(WIDTH,HEIGHT*2));
+		
 		frame.add(scrollPane);
+		frame.add(addTask);
+		
 		frame.pack();
-		frame.setLocationRelativeTo(null);
+		//frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
 	}
 
 	public void arrangeList() {
