5532366cb5b728f64ca2edb0fbc5b06382a41404
suttonr75 <suttonr75@gmail.com>
2017-03-16 09:08:09 -0400

    put in serialization code

--- ToDoList/src/Main.java
+++ ToDoList/src/Main.java
@@ -1,3 +1,9 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,6 +31,35 @@ public class Main extends Application{
             primaryStage.show();
         } catch (Exception ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+		//import code the saved to do list is saved in tdl
+		ToDoList tdl = new ToDoList();
+		try {
+			FileInputStream fileIn = new FileInputStream("src/" + "todolist" + ".java");
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			tdl = (ToDoList) in.readObject();
+			in.close();
+			fileIn.close();
+			MainMenu menu = new MainMenu(tdl);
+		} catch (FileNotFoundException e1) {
+			MainMenu a = new MainMenu(null);
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		//export code
+		try
+        {
+			FileOutputStream fileOut = new FileOutputStream("src/" + "todolist" + ".java");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(new ToDoList()); // change this to the toDoList you are trying to save
+			out.close();
+			fileOut.close();
+        }catch(IOException i)
+        {
+            i.printStackTrace();
         }
 
 	}
