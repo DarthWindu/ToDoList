eff9873b97c095551139993772165227355df7ad
More-Morphine <joe@dipasquale.org>
2017-05-30 08:23:50 -0400

    Revert "Comments"
    This reverts commit 22c73f206de4cac842736c354d25446520e9bc62.

--- ToDoList/src/backend/Comment.java
+++ ToDoList/src/backend/Comment.java
@@ -4,16 +4,13 @@ import java.io.Serializable;
 import java.util.Calendar;
 
 /**
- * Did you ever hear the tragedy of Darth Plagueis the wise?
- * I thought not. It's not a story the Jedi would tell you. It's a Sith legend.
- * Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use
- * the Force to influence the midichlorians to create.. life. He had such a knowledge
- * of the dark side that he could even keep the ones he cared about from dying.
- * The dark side of the Force is a pathway to many abilities some consider to be
- * unnatural. He became so powerful... the only thing he was afraid of was losing his power,
- * which eventually, of course, he did. Unfortunately, he taught his apprentice
- * everything he knew, then his apprentice killed him in his sleep. Ironic.
- * He could save others from death, but not himself.
+ * @author Pujit
+ * 
+ * @version p 0.1 (p is for prototype, v is for version)
+ * 
+ * Extends backend.HistoryItem
+ * Represents Comment on a task
+ *
  */
 public class Comment extends HistoryItem implements Serializable {
 	private String comment;
@@ -24,14 +21,17 @@ public class Comment extends HistoryItem implements Serializable {
 		setDate(Calendar.getInstance().getTime());//Is this the correct way to set the date?
 	}
 	
-	//Is it possible to learn this power?
+	/** @author Pujit
+	 * @return comment: Returns comment the object represent
+	 */
 	public String getComment() 
 	{
 		return comment;
 	}
 	
 	/**
-	 *Not from a Jedi.
+	 * @author Pujit
+	 * @param newComment: The new comment that replaces the old one
 	 */
 	public void setComment(String newComment)
 	{
@@ -44,4 +44,3 @@ public class Comment extends HistoryItem implements Serializable {
 		return false;
 	}
 }
-
